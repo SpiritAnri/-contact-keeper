a7a48268bcb73a6e2c15e7dac7cfbe90b596adb3
DESKTOP-0VSV11Q\sag_h <sag_h13@hotmail.com>
2020-05-09 14:52:27 +0500

    add contact filter

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import uuid from 'uuid'
+import { v4 } from 'uuid'
 import ContactContext from './contactContext'
 import ContactReducer from './contactReducer'
 import {
@@ -37,14 +37,54 @@ const ContactState = props => {
         type: 'professional',
       },
     ],
+    current: null,
+    filtered: null,
   }
 
   const [state, dispatch] = useReducer(ContactReducer, initialState)
+
+  const addContact = contact => {
+    contact.id = v4()
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
+
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id })
+  }
+
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact })
+  }
+
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT })
+  }
+
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact })
+  }
+
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text })
+  }
+
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER })
+  }
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter,
       }}
     >
       {props.children}
